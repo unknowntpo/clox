fee5765571c4ca2076cf0f274419cc05a9c4871d
Chang Chen Chien <e850506@gmail.com>
2022-01-13 20:22:06 +0800

    14.5.2: Value arrays

--- value.h
+++ value.h
@@ -5,4 +5,14 @@
 
 typedef double Value;
 
+typedef struct {
+    int capacity;
+    int count;
+    Value *values;
+} ValueArray;
+
+void initValueArray(ValueArray *array);
+void writeValueArray(ValueArray *array, Value value);
+void freeValueArray(ValueArray *array);
+
 #endif
